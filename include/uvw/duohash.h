30f3f36e8adb298d260d6e75f455ac4e5ba1341f
Jeren Chen <249388+jerenchen@users.noreply.github.com>
2021-03-18 12:10:40 -0700

    updated duo impl/tests by rule of zero

--- include/uvw/duohash.h
+++ include/uvw/duohash.h
@@ -13,19 +13,8 @@ namespace uvw
     void* raw_ptr;
     std::string var_str;
 
-    ~Duohash() {}
     Duohash(void* proc = nullptr, const std::string& label = ""):
       raw_ptr(proc), var_str(label) {}
-    Duohash(const Duohash& key)
-    {
-      *this = key;
-    }
-    Duohash& operator=(const Duohash& key)
-    {
-      this->raw_ptr = key.raw_ptr;
-      this->var_str = key.var_str;
-      return *this;
-    }
 
     friend std::ostream& operator<<(std::ostream& os, const Duohash& key)
     {
--- tests/uvw/duo.cpp
+++ tests/uvw/duo.cpp
@@ -3,24 +3,74 @@
 #include <uvw.h>
 
 
-struct TestProc : uvw::Processor
-{
-    uvw::Var<double> x_;
-};
-
 TEST_CASE("Duohash...", "[duo]")
 {
-    uvw::Processor p_;
-    uvw::Var<double> v_;
+    uvw::Processor p_, q_;
 
-    uvw::Duohash key(&p_, "v");
-    REQUIRE( key.raw_ptr == &p_ );
-
-    SECTION("assignment")
+    SECTION("Assignment")
     {
+        uvw::Duohash key(&p_, "v");
+        REQUIRE( key.raw_ptr == &p_ );
+        REQUIRE( key.var_str == "v" );
+
+        auto clone = key;
+        REQUIRE( clone.raw_ptr == &p_ );
+        REQUIRE( clone.var_str == "v" );
+        REQUIRE( clone.is_null() == false );
+
         key = uvw::Duohash();
         REQUIRE( key.raw_ptr == nullptr );
         REQUIRE( key.var_str.empty() == true );
         REQUIRE( key.is_null() == true );
+
+        REQUIRE( clone.raw_ptr == &p_ );
+        REQUIRE( clone.var_str == "v" );
+        REQUIRE( clone.is_null() == false );
+    }
+
+    SECTION("Operator==")
+    {
+        REQUIRE( (uvw::Duohash(&p_, "u") == uvw::Duohash(&p_, "u")) == true );
+        REQUIRE( (uvw::Duohash(&p_, "u") == uvw::Duohash(&p_, "v")) == false );
+        REQUIRE( (uvw::Duohash(&p_, "u") == uvw::Duohash(&q_, "u")) == false );
+        REQUIRE( (uvw::Duohash(&p_, "u") == uvw::Duohash(&q_, "v")) == false );
+    }
+
+    SECTION("Nullification")
+    {
+        uvw::Duohash key(&p_, "v");
+        REQUIRE( key.raw_ptr == &p_ );
+        REQUIRE( key.var_str == "v" );
+        REQUIRE( key.is_null() == false );
+
+        key.nullify();
+        REQUIRE( key.raw_ptr == nullptr );
+        REQUIRE( key.var_str.empty() == true );
+        REQUIRE( key.is_null() == true );
+    }
+
+    SECTION("Unordered Maps")
+    {
+        std::unordered_map<uvw::Duohash, uvw::Duohash> key_maps;
+        uvw::Duohash src(&p_, "u");
+        uvw::Duohash dst(&q_, "v");
+
+        key_maps[dst] = src;
+        auto clone = key_maps[dst];
+        REQUIRE( clone.raw_ptr == &p_ );
+        REQUIRE( clone.var_str == "u" );
+
+        src = dst = uvw::Duohash();
+        REQUIRE( src.is_null() == true );
+        REQUIRE( dst.is_null() == true );
+
+        auto clone2 = key_maps[uvw::Duohash(&q_,"v")];
+        REQUIRE( clone2.raw_ptr == &p_ );
+        REQUIRE( clone2.var_str == "u" );
+
+        REQUIRE( key_maps.erase(uvw::Duohash(&q_,"v")) == 1) ;
+        REQUIRE( clone2.raw_ptr == &p_ );
+        REQUIRE( clone2.var_str == "u" );
+        REQUIRE( clone2 == clone );
     }
 }
